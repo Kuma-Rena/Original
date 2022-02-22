de6e81fb84c94996a5d022ec5020ed1aac2cba0e
Kuma-Rena <kuki.short25@gmail.com>
2022-02-22 19:25:33 +0900

    探索（要調整）
    ベッドや棚の前に来たら「スペースキーで探索」と表示し、スペースキーを押すと調べた内容が表示されるようにした。
    ただ、動いてしまうとログが消えるので要調整。
    また微妙な場所によって探索がしっかりできないことがあるので要調整

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,42 +8,53 @@ public class PlayerController : MonoBehaviour
     Animation anim;
     Animator animator = null;
     private float velocityX = 1.0f;
-    
+    public SeachItem seach;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        float inputVelocityX = 0;        
+        float inputVelocityX = 0;
         this.rigid2D = GetComponent<Rigidbody2D>();
-        anim=this.gameObject.GetComponent<Animation>();
+        anim = this.gameObject.GetComponent<Animation>();
         animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         float inputVelocityX = 0;
+        
         //Player移動＆アニメーション
-        if (Input.GetKey(KeyCode.LeftArrow))
+        //左走行（左矢印キーだけが押されているとき）
+        if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
         {
             inputVelocityX = -this.velocityX;
             animator.SetBool("left", true);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        //右走行（右矢印キーだけが押されているとき）
+        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
         {
             inputVelocityX = this.velocityX;
             animator.SetBool("right", true);
         }
+         
         else
         {
             animator.SetBool("left", false);
             animator.SetBool("right", false);
         }
-        
-        this.rigid2D.velocity = new Vector2(inputVelocityX, 0);
-    }
 
+        this.rigid2D.velocity = new Vector2(inputVelocityX, 0);
+
+
+    }
     
 
 }
+     
+
+
+
---
+++ Assets/SeachItem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeachItem : MonoBehaviour
+{
+
+    public GameObject HintObject = null;
+    Text Hint_text;
+    public static bool flag = false;
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        this.Hint_text = HintObject.GetComponent<Text>();
+        Hint_text.text = "スペースキーで調べる";
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            flag = true;
+            switch (transform.name)
+            {
+
+                case "bed":
+                    Hint_text.text = "ふかふかのふとんだ。";
+                    break;
+
+                case "chest":
+                    Hint_text.text = "棚だ。引き出しには日用品が収納されている。";
+                    break;
+
+
+                default:
+                    Hint_text.text = "";
+                    break;
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        this.Hint_text = HintObject.GetComponent<Text>();
+        Hint_text.text = "";
+    }
+
+}
